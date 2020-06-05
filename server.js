e64e4127687a057efaba075bc176588804426458
Sabelo <sabelo.shabalala@bankzero.co.za>
2020-06-05 09:55:04 +0200

    postinstall

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname + '/dist/registration-simulator-app'));
+
+app.listen(process.env.PORT || 8080);
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/registration-simulator-app/index.html'));
+});
+
+console.log('Console listening');
